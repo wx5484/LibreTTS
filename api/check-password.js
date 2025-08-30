60d701f0ee16a207f5bc957c6119034284cc2b1b
PaceCN <52063709+wx5484@users.noreply.github.com>
2025-08-30 17:26:26 +0800

    Update check-password.js

--- api/check-password.js
+++ api/check-password.js
@@ -1,4 +1,13 @@
 export default function handler(req, res) {
   const envPass = process.env.PASSWORD || '';
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   res.status(200).json({ requirePassword: !!envPass });
 }
